54acec62143efc2c7814fc85ecafe1d484024310
bobby.li <lihan0603@hupu.com>
2019-07-12 14:23:57 +0800

    [PEELER-JS]: 0.0.5

--- src/addListener/index.ts
+++ src/addListener/index.ts
@@ -1,41 +1,35 @@
-type ValueOf<T> = T[keyof T]
+type PickOne<T, K extends keyof T> = T[K]
+type AnyEventName = keyof WindowEventMap;
+type EventListenerCallback<T extends AnyEventName> = (e: PickOne<WindowEventMap, T>) => any;
+type DOMType = Window | Document | HTMLElement;
+
 interface ListenerOption {
   capture?: boolean;
   passive?: boolean;
   once?: boolean;
 }
 
-type AnyEvent = ValueOf<WindowEventMap>
-interface AnyEventListenerCallback {
-  (e: AnyEvent): any;
-}
-
-type EventListenerCallback = AnyEventListenerCallback;
-type DOMType = Window | Document | HTMLElement;
-type AddListener = (event: string, fn: EventListenerCallback, dom: DOMType, option?: ListenerOption) => void;
-type AddListener_IE = (event: string, fn: EventListenerCallback, dom: DOMType) => void;
-
 /**
- * @param {string} event event name
+ * @param {AnyEventName} event event name
  * @param {function} fn event callback
  * @param {object} dom event dom
  * @param {ListenerOption} option option contain captrue, passive, once
  */
-export const addListener: AddListener | AddListener_IE = (function () {
+export const addListener = (function () {
   if (typeof window === 'undefined') return function (): void {}
 
   if (!window.addEventListener) {
-    return function (event: string, fn: EventListenerCallback, dom: DOMType): void {
+    return function <T extends AnyEventName>(event: T, fn: EventListenerCallback<T>, dom: DOMType): void {
       const eventDOM: any = dom || window
       eventDOM.attachEvent(`on${event}`, fn)
     }
   }
 
-  return function (event: string, fn: EventListenerCallback, dom: DOMType, option: ListenerOption = {}): void {
+  return function <T extends AnyEventName>(event: T, fn: EventListenerCallback<T>, dom: DOMType, option: ListenerOption = {}): void {
     const eventDOM = dom || window
     const { capture = false, passive = false, once = false } = option
 
-    eventDOM.addEventListener(event, fn, {
+    eventDOM.addEventListener(event, fn as EventListenerOrEventListenerObject, {
       capture,
       passive,
       once
--- src/removeListener/index.ts
+++ src/removeListener/index.ts
@@ -1,13 +1,7 @@
-type ValueOf<T> = T[keyof T]
-type AnyEvent = ValueOf<WindowEventMap>
-interface AnyEventListenerCallback {
-  (e: AnyEvent): any;
-}
-
-type EventListenerCallback = AnyEventListenerCallback;
+type PickOne<T, K extends keyof T> = T[K]
+type AnyEventName = keyof WindowEventMap;
+type EventListenerCallback<T extends AnyEventName> = (e: PickOne<WindowEventMap, T>) => any;
 type DOMType = Window | Document | HTMLElement;
-type RemoveListener = (event: string, fn: EventListenerCallback, dom: DOMType, useCapture?: boolean) => void;
-type RemoveListener_IE = (event: string, fn: EventListenerCallback, dom: DOMType) => void;
 
 /**
  * @param {string} event event name
@@ -15,19 +9,19 @@ type RemoveListener_IE = (event: string, fn: EventListenerCallback, dom: DOMType
  * @param {object} dom event dom
  * @param {boolean} useCapture bubble or capture
  */
-export const removeListener: RemoveListener | RemoveListener_IE = (function () {
+export const removeListener = (function () {
   if (typeof window === 'undefined') return function (): void {}
 
   if (!window.removeEventListener) {
-    return function (event: string, fn: EventListenerCallback, dom: DOMType): void {
+    return function <T extends AnyEventName>(event: string, fn: EventListenerCallback<T>, dom: DOMType): void {
       const eventDOM: any = dom || window
       eventDOM.detachEvent(`on${event}`, fn)
     }
   }
 
-  return function (event: string, fn: EventListenerCallback, dom: DOMType, useCapture: boolean = false): void {
+  return function <T extends AnyEventName>(event: string, fn: EventListenerCallback<T>, dom: DOMType, useCapture: boolean = false): void {
     const eventDOM = dom || window
-    eventDOM.removeEventListener(event, fn, useCapture)
+    eventDOM.removeEventListener(event, fn as EventListenerOrEventListenerObject, useCapture)
   }
 })()
 
