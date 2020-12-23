f0c30c62904ae8ddefd889d1475cb5ee6c2a0d92
bobby.li <lihan0603@hupu.com>
2020-12-23 15:44:01 +0800

    [PEELER-JS]: 0.3.10

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -28,6 +28,23 @@ const colorPlates = {
   success: 'color: white; background-color: #95B46A'
 };
 
+const nodeColorPlates = {
+  font: {
+    log: '\x1b[30m',
+    info: '\x1b[37m',
+    warn: '\x1b[30m',
+    error: '\x1b[37m',
+    success: '\x1b[37m'
+  },
+  bg: {
+    log: '\x1b[47m',
+    info: '\x1b[44m',
+    warn: '\x1b[43m',
+    error: '\x1b[41m',
+    success: '\x1b[42m'
+  }
+};
+
 export class Logger {
   private debug: boolean | undefined;
   private logLevel: TlogLevel | undefined;
@@ -147,13 +164,14 @@ export class Logger {
    * @return {void}
    */
   private _logOptimize(method: 'log' | 'info' | 'warn' | 'error' | 'success', ...msg: TlogMsg[]): void {
+    const inBrowser = typeof window !== 'undefined';
     // eslint-disable-next-line no-console
     const logger: Function = method === 'success' ? console.log.bind(console) : console[method].bind(console);
-    const prefix = `%c ${method.charAt(0).toUpperCase()}${method.slice(1)} `;
+    const prefix = inBrowser ? [`%c ${method.charAt(0).toUpperCase()}${method.slice(1)} `, colorPlates[method]] : [`${nodeColorPlates.font[method]}${nodeColorPlates.bg[method]}%s\x1b[0m`, ` ${method.charAt(0).toUpperCase()}${method.slice(1)} `];
     const canTable = msg.every(v => isType('object')(v) || isType('array')(v));
     try {
       if (console.table && canTable) {
-        logger(`${prefix}`, colorPlates[method], `[${this.logPrefix}] (${getLocalDate()}):`);
+        logger(...prefix, `[${this.logPrefix}] (${getLocalDate()}):`);
         console.table(msg);
         return;
       }
@@ -168,7 +186,7 @@ export class Logger {
       return m;
     });
 
-    logger(`${prefix}`, colorPlates[method], `[${this.logPrefix}]:`, ...msg, `(${getLocalDate()})`);
+    logger(...prefix, `[${this.logPrefix}]:`, ...msg, `(${getLocalDate()})`);
   }
 }
 
